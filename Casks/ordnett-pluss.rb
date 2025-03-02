26b53e4150d05f5dead4ddf0e9f5c143bfb4a50f
LilleAila <olai.solsvik@gmail.com>
2025-03-02 13:15:54 +0100

    more packaging of packages

--- Casks/ordnett-pluss.rb
+++ Casks/ordnett-pluss.rb
@@ -1,12 +1,18 @@
 cask "ordnett-pluss" do
   version "3.3.5"
 
-  url "https://stordnettplussprod.blob.core.windows.net/packages/ordnettpluss-#{version}-vestland_fylkeskommune.dmg"
+  url "https://stordnettplussprod.blob.core.windows.net/packages/ordnettpluss-#{version}-vestland_fylkeskommune.dmg",
+    verified: "stordnettplussprod.blob.core.windows.net"
   sha256 "eb835aef194412ede8e9267c2088f96a1e5c78495bce43101359e91c622566df"
 
   name "Ordnett pluss"
   desc "Ordbøker"
   homepage "https://www.skoleportalen.no/programvare/velg-maskintype/mac/"
 
-  pkg "OrdnettPluss3.app"
+  livecheck do
+    skip "Version check is not supported for this cask"
+  end
+
+  pkg "OrdnettPluss3.pkg"
+  uninstall pkgutil: "com.vendor.OrdnettPluss3"
 end
